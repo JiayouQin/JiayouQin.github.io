e040d6d02322f4328f9c231e1564e3b0f481fb3f
Jiayou Qin <46877249+JiayouQin@users.noreply.github.com>
2023-10-18 23:02:54 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("canvas")
+canvas.style.background = "#ff8"
+let context = canvas.getContext("2d")
+// var h = window.innerHeight
+// var w = window.innerWidth
+var h = 600
+var w = 800
+canvas.height = h
+canvas.width = w
+
+
+context.fillRect(0,0,100,100)
+
